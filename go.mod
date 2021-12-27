9109fa2f4f44dbe9dbb9b4bb27bc57c4f4005efb
denisotari <denis@otari.pro>
2021-12-27 21:57:55 +0300

    simple redis cache

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module redis_on_practice
 
 go 1.16
 
-require github.com/go-redis/redis/v8 v8.11.4
+require (
+	github.com/brianvoe/gofakeit/v6 v6.10.0 // indirect
+	github.com/go-redis/redis/v8 v8.11.4
+	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
+)
--- main.go
+++ main.go
@@ -2,26 +2,62 @@ package main
 
 import (
 	"context"
-	"errors"
+	"encoding/json"
 	"fmt"
+	"github.com/brianvoe/gofakeit/v6"
 	"github.com/go-redis/redis/v8"
+	. "github.com/logrusorgru/aurora"
+	"math/rand"
+	"strconv"
 	"time"
 )
 
-type rateLimiter struct {
-	ttl       time.Duration //
-	threshold int           // rate limiting threshold
-	client    *redis.Client
+const (
+	maxResponseMsec = 1000
+	usersCount      = 50
+	requestsCount   = 200
+	cacheTTL        = 200 //seconds
+)
+
+var rnd *rand.Rand
+
+type User struct {
+	ID    int    `json:"id" fake:"{number:1,1000}"`
+	Name  string `json:"name" fake:"{firstname}"`
+	Email string `json:"email" fake:"{email}"`
+	Phone string `json:"phone" fake:"{phone}"`
 }
 
-func (rl *rateLimiter) Rate(ctx context.Context, key string) error {
-	val, _ := rl.client.Get(ctx, key).Int()
-	if val > rl.threshold {
-		return errors.New("max rate limiter reached, please try againg later")
+type cache struct {
+	ttl    time.Duration
+	client *redis.Client
+	ctx    context.Context
+}
+
+func init() {
+	src := rand.NewSource(time.Now().UnixNano())
+	rnd = rand.New(src)
+}
+
+//userRequest return same data on same userID
+func userRequest(userID string) ([]byte, error) {
+	var user User
+	seedID, _ := strconv.ParseInt(userID, 10, 64)
+	gofakeit.Seed(seedID)
+	//random sleep
+	time.Sleep(time.Duration(rnd.Intn(maxResponseMsec)) * time.Millisecond)
+	//filling up structure
+	_ = gofakeit.Struct(&user)
+	return json.Marshal(user)
+}
+
+func (ch *cache) GetUserFromCache(userId string) (User, error) {
+	byteUser, err := ch.client.Get(ch.ctx, userId).Bytes()
+	if err != nil {
+		return User{}, err
 	}
-	rl.client.Incr(ctx, key)
-	rl.client.Expire(ctx, key, rl.ttl)
-	return nil
+	cached := toJson(byteUser)
+	return cached, nil
 }
 
 func main() {
@@ -29,29 +65,61 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	fmt.Println("Connected to redis: success")
-	rLimiter := rateLimiter{
-		ttl:       time.Second * 10,
-		threshold: 10,
-		client:    redisClient,
-	}
-	ctx := context.Background()
 
-	var i, j int
-	for {
-		if i > 50 {
-			break
-		}
-		err := rLimiter.Rate(ctx, "sample")
-		if err != nil {
-			fmt.Printf("%v. Try: %v. Wait...\n", err, j)
-			time.Sleep(100 * time.Millisecond)
-			j++
-		} else {
-			fmt.Printf("%v. Some action\n", i)
-			j = 0
-			i++
-		}
+	cache := cache{
+		ttl:    cacheTTL * time.Second,
+		client: redisClient,
+		ctx:    context.Background(),
 	}
+
+	for i := 0; i < requestsCount; i++ {
+		//getting random user id
+		usrID := strconv.Itoa(rnd.Intn(usersCount))
+
+		//resulted struct for user
+		var resultUser User
+
+		//flag for checking using cache
+		userFromCache := true
+
+		startTime := time.Now()
+		resultUser, err := cache.GetUserFromCache(usrID)
+		if err != nil {
+			userFromCache = false
+			//making lagging request
+			body, err := userRequest(usrID)
+			if err != nil {
+				fmt.Printf("error occurs %v/n", err)
+			}
+			//setting up cache
+			cacheErr := cache.client.Set(cache.ctx, usrID, body, cache.ttl).Err()
+			if cacheErr != nil {
+				fmt.Printf("cache error occurs %v/n", cacheErr)
+			}
+			resultUser = toJson(body)
+		}
+
+		//colored logs
+		lag := time.Now().Sub(startTime)
+		coloredFlag := Red("REQUESTED")
+		coloredTime := Red(lag)
+		if userFromCache {
+			coloredFlag = Green("CACHED")
+			coloredTime = Green(coloredTime)
+		}
+		fmt.Printf("id: %v, lag: %v cache: %v response: %+v \n", Yellow(usrID), coloredTime, coloredFlag, resultUser)
+	}
+
+	//flush all cache
+	cache.client.FlushAll(cache.ctx)
+}
+
+func toJson(val []byte) User {
+	user := User{}
+	err := json.Unmarshal(val, &user)
+	if err != nil {
+		panic(err)
+	}
+	return user
 }
